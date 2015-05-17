fa9d3308ce08d60962d80ae63bd1b015a4f6f269
lgraubner <graubnla@gmail.com>
2015-05-17 18:20:46 +0200

    initial commit

---
+++ crawler.js
@@ -0,0 +1,78 @@
+var Crawler = require("simplecrawler"),
+    _ = require("lodash"),
+    moment = require("moment"),
+    fs = require("fs"),
+    builder = require("xmlbuilder");
+
+
+var chunk = [],
+    args = process.argv.slice(2),
+    c = new Crawler(args[0]);
+
+c.initialPath = "/";
+c.initialPort = 80;
+c.initialProtocol = "http";
+c.maxDepth = 10;
+c.stripQuerystring = true;
+
+c.on("fetchcomplete", function(item) {
+    if (!_.has(chunk, item.url)) {
+        chunk.push({
+            loc: item.url,
+            // TODO: date ersetzen, da deprecated?
+            lastmod: moment(new Date(item.stateData.headers["last-modified"])).format("YYYY-MM-DD"),
+            // TODO: calculate changefreq
+            //changefreq: "always",
+            priority: round((1 - ((item.depth - 1) / 10)), 2)
+        });
+    }
+
+    console.log(item.url);
+});
+
+c.on("fetcherror", function(item, response) {
+    console.log(response);
+});
+
+c.on("complete", function() {
+    console.log("Done!");
+
+    var xml = builder.create("urlset", { version: "1.0", encoding: "UTF-8" }).att("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+    _.forIn(chunk, function(value, key) {
+        xml.ele("url").ele("loc", value.loc).up().ele("lastmod", value.lastmod).up().ele("changefreq", value.changefreq).up().ele("priority", value.priority);
+    });
+
+    var map = xml.end({ pretty: true, indent: '    ', newline: "\n" });
+
+    fs.writeFile("sitemap.xml", map, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
+
+c.addFetchCondition(function(parsedURL) {
+    // @TODO: add fonts
+    return !parsedURL.path.match(/\.(pdf|json|xml|gif|jpg|jpeg|png|svg|css|js|rss|atom|ico|ogg|bmp|webp|mp4|webm)$/i);
+});
+
+c.start();
+
+function round(value, exp) {
+    if (typeof exp === 'undefined' || +exp === 0)
+        return Math.round(value);
+
+    value = +value;
+    exp  = +exp;
+
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+        return NaN;
+
+    // Shift
+    value = value.toString().split('e');
+    value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+}
