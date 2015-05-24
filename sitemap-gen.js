f6d08540c9917ac6b29837453310887727081bb8
lgraubner <graubnla@gmail.com>
2015-05-24 15:41:39 +0200

    removed unused function

--- sitemap-gen.js
+++ sitemap-gen.js
@@ -85,23 +85,3 @@ var misc = c.addFetchCondition(function(parsedURL) {
 });
 
 c.start();
-
-// helper function for rounding
-function round(value, exp) {
-    if (typeof exp === 'undefined' || +exp === 0)
-        return Math.round(value);
-
-    value = +value;
-    exp  = +exp;
-
-    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
-        return NaN;
-
-    // Shift
-    value = value.toString().split('e');
-    value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
-
-    // Shift back
-    value = value.toString().split('e');
-    return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
-}
