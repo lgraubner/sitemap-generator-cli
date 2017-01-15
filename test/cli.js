e67230edb09ed15f00168411d189d24078979fed
Lars Graubner <mail@larsgraubner.de>
2017-01-15 18:19:38 +0100

    update dependencies

--- test/cli.js
+++ test/cli.js
@@ -16,7 +16,7 @@ test.cb.before(function (t) {
 test.cb('should return null for invalid URL\'s', function (t) {
   t.plan(3);
 
-  exec('node ../cli.js invalid', function (error, stdout, stderr) {
+  exec('node cli.js invalid', function (error, stdout, stderr) {
     t.is(error, null, 'no error');
     t.is(stderr, '');
     t.regex(stdout, /^null/);
@@ -28,7 +28,7 @@ test.cb('should return null for invalid URL\'s', function (t) {
 test.cb('should return valid sitemap', function (t) {
   t.plan(6);
 
-  exec('node ../cli.js ' + baseUrl + ':' + port, function (error, stdout, stderr) {
+  exec('node cli.js ' + baseUrl + ':' + port, function (error, stdout, stderr) {
     t.is(error, null, 'no error');
     t.is(stderr, '', 'no error messages');
     // sitemap
@@ -46,7 +46,7 @@ test.cb('should restrict crawler to baseurl if option is enabled', function (t) 
   t.plan(3);
 
   // eslint-disable-next-line
-  exec('node ../cli.js ' + baseUrl + ':' + port + '/subpage --baseurl', function (error, stdout, stderr) {
+  exec('node cli.js ' + baseUrl + ':' + port + '/subpage --baseurl', function (error, stdout, stderr) {
     t.is(error, null, 'no error');
     t.is(stderr, '', 'no error messages');
     var regex = new RegExp('http:\/\/' + baseUrl + ':' + port + '/<');
@@ -59,7 +59,7 @@ test.cb('should restrict crawler to baseurl if option is enabled', function (t) 
 test.cb('should include query strings if enabled', function (t) {
   t.plan(5);
 
-  exec('node ../cli.js ' + baseUrl + ':' + port + ' --query', function (error, stdout, stderr) {
+  exec('node cli.js ' + baseUrl + ':' + port + ' --query', function (error, stdout, stderr) {
     t.is(error, null, 'no error');
     t.is(stderr, '', 'no error messages');
     t.not(stdout, '', 'stdout is not empty');
@@ -75,7 +75,7 @@ test.cb('should include query strings if enabled', function (t) {
 test.cb('should log requests if dry mode is enabled', function (t) {
   t.plan(4);
 
-  exec('node ../cli.js ' + baseUrl + ':' + port + ' --dry', function (error, stdout, stderr) {
+  exec('node cli.js ' + baseUrl + ':' + port + ' --dry', function (error, stdout, stderr) {
     t.is(error, null, 'no error');
     t.is(stderr, '', 'no error messages');
     t.not(stdout, '', 'stdout is not empty');
