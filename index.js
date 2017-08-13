2cf132dc71f7e671d8d23596fd87181c39c77247
Lars Graubner <mail@larsgraubner.de>
2017-08-13 03:12:16 +0200

    updated sitemap-generator

--- index.js
+++ index.js
@@ -25,24 +25,17 @@ function sitemapFactory() {
     filepath: program.args[1],
   });
 
-  let added = 0;
-  let ignored = 0;
-  let errored = 0;
-
   // add event listeners to crawler if verbose mode enabled
   if (program.verbose) {
     generator.on('add', url => {
-      added += 1;
       console.log('[', chalk.green('ADD'), ']', chalk.gray(url));
     });
 
     generator.on('ignore', url => {
-      ignored += 1;
       console.log('[', chalk.cyan('IGN'), ']', chalk.gray(url));
     });
 
     generator.on('error', error => {
-      errored += 1;
       console.error(
         '[',
         chalk.red('ERR'),
@@ -52,24 +45,13 @@ function sitemapFactory() {
     });
   }
 
-  generator.on('done', () => {
+  generator.on('done', ({ added, ignored, errored }) => {
     // show stats if dry mode
     if (program.verbose) {
       const message =
         'Added %s pages, ignored %s pages, encountered %s errors.';
       const stats = [chalk.white(message), added, ignored, errored];
-
-      // no results => site not found
-      if (added === 0 && errored === 0 && ignored === 0) {
-        console.error(
-          chalk.red('Site "%s" could not be found'),
-          program.args[0]
-        );
-        // exit with error
-        process.exit(1);
-      } else {
-        console.log.apply(this, stats);
-      }
+      console.log.apply(this, stats);
     }
 
     process.exit(0);
