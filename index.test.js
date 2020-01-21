9e2cf0f80271fe42b1579c648880b35bb183ed84
Wbeimar Logatto <wblomo22@gmail.com>
2020-01-21 08:13:26 -0500

    feat: lastmod priority changefreq options (#42)
    * Add new options lastmod, priprity and changefred
    
    * add new options to readme

--- index.test.js
+++ index.test.js
@@ -9,35 +9,39 @@ afterEach(done => {
   });
 });
 
-test(
-  'should create sitemap file',
-  () => {
-    expect.assertions(1);
+test('should create sitemap file', () => {
+  expect.assertions(1);
 
-    return execa('node', [
-      'index.js',
-      'http://example.com',
-      'sitemap.xml',
-    ]).then(() => {
+  return execa('node', ['index.js', 'http://example.com', 'sitemap.xml']).then(
+    () => {
       expect(() => fs.accessSync('sitemap.xml')).not.toThrow();
-    });
-  },
-  20000
-);
+    }
+  );
+}, 20000);
 
-test(
-  'should write to stdout in verbose mode',
-  () => {
-    expect.assertions(1);
+test('should write to stdout in verbose mode', () => {
+  expect.assertions(1);
 
-    return execa('node', [
-      'index.js',
-      'http://example.com',
-      'sitemap.xml',
-      '--verbose',
-    ]).then(result => {
-      expect(result.stdout).not.toBe('');
+  return execa('node', [
+    'index.js',
+    'http://example.com',
+    'sitemap.xml',
+    '--verbose'
+  ]).then(result => {
+    expect(result.stdout).not.toBe('');
+  });
+}, 20000);
+
+test('should adds last-mod header to xml', () => {
+  return execa('node', [
+    'index.js',
+    'http://example.com',
+    'sitemap.xml',
+    '--last-mod'
+  ]).then(() => {
+    fs.readFile('sitemap.xml', 'utf8', (err, data) => {
+      if (err) throw err;
+      expect(data).toContain('<lastmod>');
     });
-  },
-  20000
-);
+  });
+}, 20000);
